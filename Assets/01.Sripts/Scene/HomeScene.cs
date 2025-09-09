05dc18e7c7d084f3cb8de8bce7846fe6f2e83544
minjeparkk <zone8572@gmail.com>
2025-09-09 20:28:08 +0900

    Chore : 씬정리

--- Assets/01.Sripts/Scene/HomeScene.cs
+++ Assets/01.Sripts/Scene/HomeScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntroScene : SceneBase
+public class HomeScene : SceneBase
 {
     protected override void Awake()
     {
