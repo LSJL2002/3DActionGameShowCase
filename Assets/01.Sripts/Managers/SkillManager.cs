da44f596e1bd0e64292d27872277686652a6ab38
charliebrown1994 <leesungyul@outlook.com>
2025-09-10 22:39:10 +0900

    Fix : 스킬 메니저 스크립트 주석 추가

--- Assets/01.Sripts/Managers/SkillManager.cs
+++ Assets/01.Sripts/Managers/SkillManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class SkillManager : Singleton<SkillManager>
 {
-    // ������ 
-    // ���������Ǿ�
-
-    // 1�� ��ų Ŭ����, 2�� ��ų Ŭ����.... 
+    // 스킬 클래스 참조만 
+    // 오버랩스피어
+    // 파티클 사용 하기 (몬스터를 부디)
+    // 1번 스킬 클래스, 2번 스킬 클래스.... 
 }
