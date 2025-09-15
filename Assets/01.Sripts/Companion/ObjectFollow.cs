5a6380e8f9e67e3b2f6ea08f7ab1d2e647145aa7
charliebrown1994 <leesungyul@outlook.com>
2025-09-15 15:49:41 +0900

    Fix : 조력자 이동에 틱 발생 수정 중

--- Assets/01.Sripts/Companion/ObjectFollow.cs
+++ Assets/01.Sripts/Companion/ObjectFollow.cs
@@ -1,11 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectFollow : MonoBehaviour
 {
     [Header("FollowObject")]
     [SerializeField] private Transform targetObject;
+
+    // 회전과 이동에 속도를 적용합니다.
+    [Header("Speed")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
 
     private void Update()
     {
@@ -14,10 +17,16 @@ public class ObjectFollow : MonoBehaviour
 
     void FollowObject()
     {
-        if (targetObject != null)
-        {
-            this.transform.position = targetObject.position;
-            this.transform.rotation = targetObject.rotation;
-        }
+        // 단순 위치 이동 (순간이동하는 느낌) 
+        //if (targetObject != null)
+        //{
+        //    this.transform.position = targetObject.position;
+        //    this.transform.rotation = targetObject.rotation;
+        //}
+
+        // 부드러운 위치 이동
+        if ( targetObject == null ) return;
+        this.transform.position = Vector3.Lerp(transform.position, targetObject.position, Time.deltaTime * moveSpeed);
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, targetObject.rotation, Time.deltaTime * rotationSpeed);
     }
 }
