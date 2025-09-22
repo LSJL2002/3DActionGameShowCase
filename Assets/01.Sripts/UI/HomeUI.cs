3691964f7c290d23a29074f6e87c470628813133
minjeparkk <zone8572@gmail.com>
2025-09-22 12:42:33 +0900

    Feat : 홈씬 수정 완료

--- Assets/01.Sripts/UI/HomeUI.cs
+++ Assets/01.Sripts/UI/HomeUI.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HomeUI : UIBase
 {
+    [SerializeField] private CanvasGroup newGameButton;
+    [SerializeField] private CanvasGroup loadGameButton;
+    [SerializeField] private CanvasGroup optionButton;
+    [SerializeField] private CanvasGroup quitButton;
+
     public async void OnClickButton(string str)
     {
         switch(str)
@@ -31,5 +38,22 @@ public class HomeUI : UIBase
 
         // 현재 팝업창 닫기
         Hide();
+    }
+
+    protected override void Awake()
+    {
+        newGameButton.alpha = 0f;
+        loadGameButton.alpha = 0f;
+        optionButton.alpha = 0f;
+        quitButton.alpha = 0f;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        newGameButton.DOFade(1f, 1.0f);
+        loadGameButton.DOFade(1f, 2.0f);
+        optionButton.DOFade(1f, 3.0f);
+        quitButton.DOFade(1f, 4.0f);
     }
 }
