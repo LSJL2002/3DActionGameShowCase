b233529435caabf2f4e532505cd55173627a9f3a
charliebrown1994 <leesungyul@outlook.com>
2025-09-24 20:40:06 +0900

    Feat : 대화 SO 만들기

--- Assets/01.Sripts/Companion/TalkSystem/TutorialUI.cs
+++ Assets/01.Sripts/Companion/TalkSystem/TutorialUI.cs
@@ -8,11 +8,14 @@ using TMPro;
 public class TutorialUI : UIBase
 {
     public TMP_Text talkText;
-    public GameObject tutoriaUI;
 
-    public void OnClick()
+
+    // 창이 켜질 때 자동으로 실행됨
+    protected override void OnEnable()
     {
-        tutoriaUI.SetActive(true);
-        talkText.text = "안녕 나는 vixi야 잘 부탁해";
+        base.OnEnable(); // UIBase의 OnEnable 호출 (없어도 되지만 안전하게 남겨두기)
+
+        if (talkText != null)
+            talkText.text = "안녕 나는 vixi야 잘 부탁해";
     }
 }
