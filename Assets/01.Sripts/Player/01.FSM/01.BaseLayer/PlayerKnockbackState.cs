4748793c46a0b4a5d3eef0889b34ddf49d4568a3
Heonjjj <kt3238@naver.com>
2025-09-29 17:38:33 +0900

    Fix: 기절수정

--- Assets/01.Sripts/Player/01.FSM/01.BaseLayer/PlayerKnockbackState.cs
+++ Assets/01.Sripts/Player/01.FSM/01.BaseLayer/PlayerKnockbackState.cs
@@ -7,7 +7,6 @@ public class PlayerKnockbackState : Istate
 {
     private readonly PlayerStateMachine stateMachine;
 
-    private readonly float layerBlendSpeed;
     private readonly int knockLayerIndex;
 
     private Vector3 knockDirection;
@@ -15,13 +14,9 @@ public class PlayerKnockbackState : Istate
     private float duration;
     private float elapsed;
 
-    private bool targetLayerOn = false;
-    private float currentLayerWeight = 0f;
-
     public PlayerKnockbackState(PlayerStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
-        this.layerBlendSpeed = layerBlendSpeed;
 
         knockLayerIndex = stateMachine.Player.Animator.GetLayerIndex("Overall/Toggle_HitStopLayer");
     }
@@ -39,10 +34,11 @@ public class PlayerKnockbackState : Istate
 
     public void Enter()
     {
-        targetLayerOn = true;
         // 애니메이션 트리거
         var anim = stateMachine.Player.Animator;
         anim.SetTrigger(stateMachine.Player.AnimationData.KnockbackParameterHash);
+        anim.SetLayerWeight(knockLayerIndex, 1);
+
 
         // 캐릭터를 넉백 방향으로 바라보게 회전 고정
         stateMachine.Player.transform.forward = knockDirection;
@@ -53,7 +49,9 @@ public class PlayerKnockbackState : Istate
 
     public void Exit()
     {
-        targetLayerOn = false;
+        var anim = stateMachine.Player.Animator;
+        anim.SetLayerWeight(knockLayerIndex, 1);
+
         stateMachine.IsKnockback = false;
     }
 
@@ -69,24 +67,11 @@ public class PlayerKnockbackState : Istate
             // 넉백 종료 → Idle로 복귀
             stateMachine.ChangeState(stateMachine.IdleState);
         }
-        UpdateLayerWeight();
     }
 
     public void PhysicsUpdate()
     {
         // 넉백 방향으로 밀기
         stateMachine.Player.Controller.Move(knockDirection * knockForce * Time.deltaTime);
-    }
-
-    private void UpdateLayerWeight()
-    {
-        float target = targetLayerOn ? 1f : 0f;
-        currentLayerWeight = Mathf.MoveTowards(
-            currentLayerWeight,
-            target,
-            layerBlendSpeed * Time.deltaTime
-        );
-
-        stateMachine.Player.Animator.SetLayerWeight(knockLayerIndex, currentLayerWeight);
     }
 }
