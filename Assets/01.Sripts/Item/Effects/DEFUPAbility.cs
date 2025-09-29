4f72759f416b1063464d3348a25489f811caf8da
minjeparkk <zone8572@gmail.com>
2025-09-29 10:30:43 +0900

    Feat : 아이템 어빌리티 SO 구조 수정
    아이템 매니저를 통한 작동 구조에서 각 SO에서 작동하도록 수정 (스킬공격력, 상태이상지속시간, 쿨다운증가는 아직 미구현)

---
+++ Assets/01.Sripts/Item/Effects/DEFUPAbility.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemAbility", menuName = "Item Abilities/DEFUP")]
+public class DEFUPAbility : ItemAbility
+{
+    // DOTween Sequence 참조를 저장
+    private Sequence DEFUPSequence;
+
+    public override void Use(ItemData itemData)
+    {
+        PlayerStats stats = PlayerManager.Instance.Stats;
+        float defupPercentage = itemData.effectValue; // 추가 스탯
+        float duration = itemData.duration;
+        float totalDEFUPAmount = stats.Defense.Value * defupPercentage / 100; // 총 증가량 계산: 공격력의 n%
+
+        // 이전 Sequence가 있다면 제거 (중복 사용 방지)
+        DEFUPSequence?.Kill();
+
+        // 새로운 Sequence 생성
+        DEFUPSequence = DOTween.Sequence();
+
+        DEFUPSequence.AppendCallback(() => stats.AddModifier(StatType.Defense, totalDEFUPAmount));
+        DEFUPSequence.AppendInterval(duration);
+        DEFUPSequence.AppendCallback(() => stats.RemoveModifier(StatType.Defense, totalDEFUPAmount));
+
+        // Sequence 재생 및 Auto파괴옵션세팅 호출
+        DEFUPSequence.SetAutoKill(true) // 기본값(true)을 명시적으로 설정
+                    .OnKill(() => DEFUPSequence = null) // Sequence가 Kill 될 때 참조 해제
+                    .Play(); // Sequence 시작
+
+        Debug.Log($"물약 사용 시작: 총 {duration}초 동안 공격력 {defupPercentage}% 증가(+{totalDEFUPAmount})");
+    }
+}
---
+++ Assets/01.Sripts/Item/Effects/MoveSPDUPAbility.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemAbility", menuName = "Item Abilities/MoveSPDUP")]
+public class MoveSPDUPAbility : ItemAbility
+{
+    // DOTween Sequence 참조를 저장
+    private Sequence movespdupSequence;
+
+    public override void Use(ItemData itemData)
+    {
+        PlayerStats stats = PlayerManager.Instance.Stats;
+        float movespdupPercentage = itemData.effectValue; // 추가 스탯
+        float duration = itemData.duration;
+        float totalMoveSPDUPAmount = stats.MoveSpeed.Value * movespdupPercentage / 100; // 총 증가량 계산: 공격력의 n%
+
+        // 이전 Sequence가 있다면 제거 (중복 사용 방지)
+        movespdupSequence?.Kill();
+
+        // 새로운 Sequence 생성
+        movespdupSequence = DOTween.Sequence();
+
+        movespdupSequence.AppendCallback(() => stats.AddModifier(StatType.MoveSpeed, totalMoveSPDUPAmount));
+        movespdupSequence.AppendInterval(duration);
+        movespdupSequence.AppendCallback(() => stats.RemoveModifier(StatType.MoveSpeed, totalMoveSPDUPAmount));
+
+        // Sequence 재생 및 Auto파괴옵션세팅 호출
+        movespdupSequence.SetAutoKill(true) // 기본값(true)을 명시적으로 설정
+                    .OnKill(() => movespdupSequence = null) // Sequence가 Kill 될 때 참조 해제
+                    .Play(); // Sequence 시작
+
+        Debug.Log($"물약 사용 시작: 총 {duration}초 동안 공격력 {movespdupPercentage}% 증가(+{totalMoveSPDUPAmount})");
+    }
+}
