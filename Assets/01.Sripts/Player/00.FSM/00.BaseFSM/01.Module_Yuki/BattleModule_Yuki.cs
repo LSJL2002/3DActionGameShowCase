82016b45a8c556274b9cc39f52033ab22a5891f1
Heonjjj <kt3238@naver.com>
2025-10-16 16:12:59 +0900

    Fix: 에러수정

--- Assets/01.Sripts/Player/00.FSM/00.BaseFSM/01.Module_Yuki/BattleModule_Yuki.cs
+++ Assets/01.Sripts/Player/00.FSM/00.BaseFSM/01.Module_Yuki/BattleModule_Yuki.cs
@@ -12,8 +12,6 @@ public class BattleModule_Yuki : BattleModule
     private SkillSubModule_Yuki skillSub;
     private AwakenSubModule_Yuki awakenSub;
 
-    public bool IsAwakened => awakenSub.IsAwakened;
-
     public BattleModule_Yuki(PlayerStateMachine sm) : base(sm)
     {
         comboSub = new ComboSubModule_Yuki(sm);
