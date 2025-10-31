8b76d1af1aa7b6ef094b713c458b02c004f02d9a
SJL2002 <sungjunlim13@gmail.com>
2025-10-31 12:06:36 +0900

    Feat : FunnelEvents 추가
    각 몬스터 90% 60% 30% 10% 0%의 이벤트 추가

--- Assets/01.Sripts/Monster/MonsterStateMachine/SmileMachine_UseFire/SmileMachine_UseFire.cs
+++ Assets/01.Sripts/Monster/MonsterStateMachine/SmileMachine_UseFire/SmileMachine_UseFire.cs
@@ -11,7 +11,13 @@ public class SmileMachine_UseFire : BaseMonster
     [Header("Fireball Effects")]
     public GameObject fireball;
     public GameObject groundFire;
-
+    
+    //Funnel Events
+    private bool hasSent90HPEvent = false;
+    private bool hasSent60HPEvent = false;
+    private bool hasSent30HPEvent = false;
+    private bool hasSent10HPEvent = false;
+    private bool hasDied = false;
     protected override void Awake()
     {
         base.Awake();
@@ -44,5 +50,51 @@ public class SmileMachine_UseFire : BaseMonster
         if (state is SmileMachine_FireShoot) return Stats.GetSkill("SmileMachine_FireShoot").skillUseRange;
 
         return Stats.AttackRange;
+    }
+    public override void OnTakeDamage(int amount)
+    {
+        base.OnTakeDamage(amount);
+
+        float hpPercent = (Stats.CurrentHP / Stats.maxHp) * 100f;
+
+        // 90%
+        if (!hasSent90HPEvent && hpPercent <= 90f)
+        {
+            hasSent90HPEvent = true;
+            AnalyticsManager.SendFunnelStep("32");
+            Debug.Log("Analytics value 32 sent.");
+        }
+
+        // 60%
+        if (!hasSent60HPEvent && hpPercent <= 60f)
+        {
+            hasSent60HPEvent = true;
+            AnalyticsManager.SendFunnelStep("33");
+            Debug.Log("Analytics value 33 sent.");
+        }
+
+        // 30%
+        if (!hasSent30HPEvent && hpPercent <= 30f)
+        {
+            hasSent30HPEvent = true;
+            AnalyticsManager.SendFunnelStep("34");
+            Debug.Log("Analytics value 34 sent.");
+        }
+
+        // 10%
+        if (!hasSent10HPEvent && hpPercent <= 10f)
+        {
+            hasSent10HPEvent = true;
+            AnalyticsManager.SendFunnelStep("35");
+            Debug.Log("Analytics value 35 sent.");
+        }
+
+        // 0%
+        if (!hasDied && hpPercent <= 0f)
+        {
+            hasDied = true;
+            AnalyticsManager.SendFunnelStep("36");
+            Debug.Log("Analytics value 36 sent");
+        }
     }
 }
--- Assets/01.Sripts/Monster/MonsterStateMachine/SpiderTractor/SpiderTractor_UseGrenade.cs
+++ Assets/01.Sripts/Monster/MonsterStateMachine/SpiderTractor/SpiderTractor_UseGrenade.cs
@@ -12,6 +12,12 @@ public class SpiderTractor_UseGrenade : BaseMonster
     [Header("Bombardment")]
     public GameObject missile;
     public Transform firePointMissile;
+    //Funnel Events
+    private bool hasSent90HPEvent = false;
+    private bool hasSent60HPEvent = false;
+    private bool hasSent30HPEvent = false;
+    private bool hasSent10HPEvent = false;
+    private bool hasDied = false;
     protected override MonsterBaseState GetStateFromEnum(States stateEnum)
     {
         switch (stateEnum)
@@ -57,12 +63,58 @@ public class SpiderTractor_UseGrenade : BaseMonster
             spinState.TriggerSpin();
         }
     }
-    
+
     public void StopSpin()
     {
         if (stateMachine.CurrentState is SpiderMachine_Spin spinState)
         {
             spinState.StopSpin();
+        }
+    }
+    public override void OnTakeDamage(int amount)
+    {
+        base.OnTakeDamage(amount);
+
+        float hpPercent = (Stats.CurrentHP / Stats.maxHp) * 100f;
+
+        // 90%
+        if (!hasSent90HPEvent && hpPercent <= 90f)
+        {
+            hasSent90HPEvent = true;
+            AnalyticsManager.SendFunnelStep("52");
+            Debug.Log("Analytics value 52 sent.");
+        }
+
+        // 60%
+        if (!hasSent60HPEvent && hpPercent <= 60f)
+        {
+            hasSent60HPEvent = true;
+            AnalyticsManager.SendFunnelStep("53");
+            Debug.Log("Analytics value 53 sent.");
+        }
+
+        // 30%
+        if (!hasSent30HPEvent && hpPercent <= 30f)
+        {
+            hasSent30HPEvent = true;
+            AnalyticsManager.SendFunnelStep("54");
+            Debug.Log("Analytics value 54 sent.");
+        }
+
+        // 10%
+        if (!hasSent10HPEvent && hpPercent <= 10f)
+        {
+            hasSent10HPEvent = true;
+            AnalyticsManager.SendFunnelStep("55");
+            Debug.Log("Analytics value 55 sent.");
+        }
+
+        // 0%
+        if (!hasDied && hpPercent <= 0f)
+        {
+            hasDied = true;
+            AnalyticsManager.SendFunnelStep("56");
+            Debug.Log("Analytics value 56 sent");
         }
     }
 }
